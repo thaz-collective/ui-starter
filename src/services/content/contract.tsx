c89a36c8af011315243609d531f33073e89b3942
Tim Hazlett <timhazlett2016@gmail.com>
2026-07-16 09:50:35 -0500

    WIP - combining shared logic

--- src/services/content/contract.tsx
+++ src/services/content/contract.tsx
@@ -15,39 +15,44 @@ export const getComponentSlugList = createServerFn({ method: 'GET' }).handler(()
     });
 });
 
-export const getComponentFrontMatterMetaBySlug = createServerFn({ method: 'POST' })
-  .validator(
-    v.object({
-      slug: v.string(),
-    }),
-  )
+const validatorSlug = v.object({
+  slug: v.string(),
+});
+
+const getComponentBySlug = createServerFn({ method: 'POST' })
+  .validator(validatorSlug)
   .handler(({ data: { slug } }) => {
     const component = allComponents.find((componentCtx) => componentCtx.slug === slug);
     if (component) {
-      return {
-        slug: component.slug,
-        title: component.title,
-        date: component.date,
-        author: component.author,
-      };
+      return component;
     }
 
     throw notFound();
   });
 
-export const getComponentMDXBySlug = createServerFn({ method: 'POST' })
-  .validator(
-    v.object({
-      slug: v.string(),
-    }),
-  )
-  .handler(({ data: { slug } }) => {
-    const component = allComponents.find((componentCtx) => componentCtx.slug === slug);
-    if (component) {
-      return {
-        mdx: component.mdx,
-      };
-    }
+export const getComponentFrontMatterMetaBySlug = createServerFn({ method: 'POST' })
+  .validator(validatorSlug)
+  .handler(async ({ data }) => {
+    const component = await getComponentBySlug({
+      data,
+    });
 
-    throw notFound();
+    return {
+      slug: component.slug,
+      title: component.title,
+      date: component.date,
+      author: component.author,
+    };
+  });
+
+export const getComponentMDXBySlug = createServerFn({ method: 'POST' })
+  .validator(validatorSlug)
+  .handler(async ({ data }) => {
+    const component = await getComponentBySlug({
+      data,
+    });
+
+    return {
+      mdx: component.mdx,
+    };
   });
