6554713bd59c26388e4727b174b726d1749b0cb4
Tim Hazlett <timhazlett2016@gmail.com>
2026-08-23 14:23:10 -0500

    WIP - adding a debounced version of text area

--- src/common/components/text-area/index.ts
+++ src/common/components/text-area/index.ts
@@ -1,5 +1,7 @@
 export { TextArea } from './components/text-area';
+export { TextAreaDebounced } from './components/text-area-debounced';
 
 export type { TextAreaProps } from './components/text-area';
+export type { TextAreaDebouncedProps } from './components/text-area-debounced';
 
 export { textAreaVariants } from './variants';
