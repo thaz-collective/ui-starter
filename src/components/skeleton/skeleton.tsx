23c6e2e18621849accde3ee32d6be7b3a76f4bca
Tim Hazlett <47572900+thazlett16@users.noreply.github.com>
2026-09-17 14:44:14 -0500

    feat: add new component skeleton (#9)
    * initial commit for skeleton
    
    * Simplify down to just pulse for now
    
    * Trying a shimmer
    
    * reverting back to the pulse instead

---
+++ src/components/skeleton/skeleton.tsx
@@ -0,0 +1,31 @@
+import type { ComponentPropsWithRef } from 'react';
+
+import type { VariantProps } from 'tailwind-variants';
+import { tv } from 'tailwind-variants';
+
+const skeletonVariants = tv({
+  base: 'animate-pulse bg-field-hover select-none motion-reduce:animate-none',
+  defaultVariants: {
+    shape: 'text',
+  },
+  variants: {
+    shape: {
+      text: 'h-4 w-full rounded-sm',
+      circle: 'aspect-square rounded-full',
+      rectangle: 'w-full rounded-md',
+    },
+  },
+});
+
+type SkeletonProps = ComponentPropsWithRef<'div'> & VariantProps<typeof skeletonVariants>;
+
+export function Skeleton(props: SkeletonProps) {
+  return (
+    <div
+      {...props}
+      role={props.role ?? 'presentation'}
+      aria-hidden={props['aria-hidden'] ?? true}
+      className={skeletonVariants({ ...props })}
+    />
+  );
+}
